b73c4b4416f23145313ba8b5d07eadd3c77838f9
duexcoast <conor.ux@gmail.com>
2023-01-03 12:12:00 -0500

    added higher order reduce function and implememted it with generics

--- sum.go
+++ sum.go
@@ -2,23 +2,19 @@ package main
 
 // Sum calculates the total from a slice of numbers
 func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 // SumOfAllTails calculates the sum of all but the first number given a collection of slices
 func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
-	return sums
+	return Reduce(numbersToSum, sumTail, []int{})
 }
